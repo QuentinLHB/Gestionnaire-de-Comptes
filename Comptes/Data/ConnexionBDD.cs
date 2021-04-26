6d4658c452463116cc30828b8d50c6b1e23af33a
Quentin Lhb <q.lehembre@gmail.com>
2021-04-26 11:42:12 +0200

    Ajout de la base de donnée.

---
+++ Comptes/Data/ConnexionBDD.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace MediaTek.Connexion
+{
+    /// <summary>
+    /// Connexion à la base de données et exécution des requêtes
+    /// </summary>
+    public class ConnexionBDD
+    {
+        /// <summary>
+        /// Unique instance de la classe
+        /// </summary>
+        private static ConnexionBDD instance = null;
+        /// <summary>
+        /// objet de connexion à la BDD à partir d'une chaîne de connexion
+        /// </summary>
+        private MySqlConnection connection;
+        /// <summary>
+        /// objet pour exécuter une requête SQL
+        /// </summary>
+        private MySqlCommand command;
+        /// <summary>
+        /// objet contenant le résultat d'une requête "select" (curseur)
+        /// </summary>
+        private MySqlDataReader reader;
+
+        /// <summary>
+        /// Constructeur privé pour créer la connexion à la BDD et l'ouvrir
+        /// </summary>
+        /// <param name="stringConnect">chaine de connexion</param>
+        private ConnexionBDD(string stringConnect)
+        {
+            try
+            {
+                connection = new MySqlConnection(stringConnect);
+                connection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Application.Exit();
+            }
+        }
+
+        /// <summary>
+        /// Crée une instance unique de la classe
+        /// </summary>
+        /// <param name="stringConnect">chaine de connexion</param>
+        /// <returns>instance unique de la classe</returns>
+        public static ConnexionBDD GetInstance(string stringConnect)
+        {
+            if (instance is null)
+            {
+                instance = new ConnexionBDD(stringConnect);
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// Exécution d'une requête autre que "select"
+        /// </summary>
+        /// <param name="stringQuery">requête autre que select</param>
+        /// <param name="parameters">dictionnire contenant les parametres</param>
+        public void ReqUpdate(string stringQuery, Dictionary<string, object> parameters)
+        {
+            try
+            {
+                command = new MySqlCommand(stringQuery, connection);
+                if (!(parameters is null))
+                {
+                    foreach (KeyValuePair<string, object> parameter in parameters)
+                    {
+                        command.Parameters.Add(new MySqlParameter(parameter.Key, parameter.Value));
+                    }
+                }
+                command.Prepare();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Exécute une requête type "select" et valorise le curseur
+        /// </summary>
+        /// <param name="stringQuery">requête select</param>
+        public void ReqSelect(string stringQuery, Dictionary<string, object> parameters)
+        {
+            try
+            {
+                command = new MySqlCommand(stringQuery, connection);
+                if (!(parameters is null))
+                {
+                    foreach (KeyValuePair<string, object> parameter in parameters)
+                    {
+                        command.Parameters.Add(new MySqlParameter(parameter.Key, parameter.Value));
+                    }
+                }
+                command.Prepare();
+                reader = command.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Tente de lire la ligne suivante du curseur
+        /// </summary>
+        /// <returns>false si fin de curseur atteinte</returns>
+        public bool Read()
+        {
+            if (reader is null)
+            {
+                return false;
+            }
+            try
+            {
+                return reader.Read();
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Retourne le contenu d'un champ dont le nom est passé en paramètre
+        /// </summary>
+        /// <param name="nameField">nom du champ</param>
+        /// <returns>valeur du champ</returns>
+        public object Field(string nameField)
+        {
+            if (reader is null)
+            {
+                return null;
+            }
+            try
+            {
+                return reader[nameField];
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Fermeture du curseur
+        /// </summary>
+        public void Close()
+        {
+            if (!(reader is null))
+            {
+                reader.Close();
+            }
+        }
+
+    }
+}
---
+++ Comptes/Data/DataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comptes.Data
+{
+    class DataAccess
+    {
+
+        readonly private static string connectionString = "server = localhost; user id = admincomptes; password=comptesQL;database=comptes";
+    
+
+    
+    }
+}
